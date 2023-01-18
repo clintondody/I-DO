c3a1625da34e84a6d56c18f7606d8238a62a8a44
Clinton Dody <clintondody@gmail.com>
2023-01-18 17:14:09 +0700

    11

--- app/src/main/java/kampus/merdeka/I/Do/app/Register_Berhasil_Activity.kt
+++ app/src/main/java/kampus/merdeka/I/Do/app/Register_Berhasil_Activity.kt
@@ -1,11 +1,23 @@
 package kampus.merdeka.I.Do.app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Register_Berhasil_Activity : AppCompatActivity() {
+
+    var berhasil : Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_berhasil)
+
+        lighStatusBar(window)
+
+        berhasil = findViewById(R.id.btn_reg_berhasil)
+
+        berhasil?.setOnClickListener {
+            startActivity(Intent(this@Register_Berhasil_Activity, login_Activity::class.java))
+        }
     }
 }
--- app/src/main/java/kampus/merdeka/I/Do/app/register_Activity.kt
+++ app/src/main/java/kampus/merdeka/I/Do/app/register_Activity.kt
@@ -6,9 +6,12 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 
 class register_Activity : AppCompatActivity() {
+
+    var daftar_akun : Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
@@ -16,6 +19,11 @@ class register_Activity : AppCompatActivity() {
 
         lighStatusBar(window)
 
+        daftar_akun = findViewById(R.id.btn_daftar)
+
+        daftar_akun?.setOnClickListener {
+            startActivity(Intent(this@register_Activity, Register_Berhasil_Activity::class.java))
+        }
 
     }
 }
