839d1aacded5c3fcaa3a9261bd035c78c8e53b69
Clinton Dody <clintondody@gmail.com>
2023-01-30 21:32:04 +0700

    15

--- app/src/main/java/kampus/merdeka/I/Do/app/login_Activity.kt
+++ app/src/main/java/kampus/merdeka/I/Do/app/login_Activity.kt
@@ -3,30 +3,35 @@ package kampus.merdeka.I.Do.app
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
 import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kampus.merdeka.I.Do.app.databinding.ActivityLoginBinding
 import kotlin.math.tan
 
 class login_Activity : AppCompatActivity() {
 
     var lupa_password : TextView? = null
-    var login : Button? = null
     var daftar : TextView? = null
     var tanpa_login : TextView? = null
+    lateinit var auth : FirebaseAuth
+    lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
 
 
         lighStatusBar(window)
-
-
+        auth = FirebaseAuth.getInstance()
 
         lupa_password = findViewById(R.id.tv_lupapw)
-        login = findViewById(R.id.btn_login)
+
         daftar = findViewById(R.id.tv_daftar)
         tanpa_login = findViewById(R.id.tv_tanpa_login)
 
@@ -34,9 +39,7 @@ class login_Activity : AppCompatActivity() {
             startActivity(Intent(this@login_Activity, lupa_password_Activity::class.java))
         }
 
-        login?.setOnClickListener {
-            startActivity(Intent(this@login_Activity, HomeActivity::class.java))
-        }
+
         daftar?.setOnClickListener {
             startActivity(Intent(this@login_Activity, register_Activity::class.java))
         }
@@ -44,7 +47,50 @@ class login_Activity : AppCompatActivity() {
         tanpa_login?.setOnClickListener {
             startActivity(Intent(this@login_Activity, HomeActivity::class.java))
         }
+
+
+        binding.btlogin.setOnClickListener{
+            val email = binding.emailLogin.text.toString()
+            val password = binding.passwordLogin.text.toString()
+
+
+            if (email.isEmpty()){
+                binding.emailLogin.error = "Email Wajib Diisi"
+                binding.emailLogin.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                binding.emailLogin.error = "Email Tidak Valid"
+                binding.emailLogin.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (password.isEmpty()){
+                binding.passwordLogin.error = "Password Wajib Diisi"
+                binding.passwordLogin.requestFocus()
+                return@setOnClickListener
+            }
+
+
+            LoginFirebase(email,password)
+        }
+
     }
+
+    private fun LoginFirebase(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email,password)
+            .addOnCompleteListener(this){
+                if (it.isSuccessful){
+                    Toast.makeText(this, "Selamat datang $email", Toast.LENGTH_SHORT).show()
+                    val intent = Intent (this,HomeActivity::class.java)
+                    startActivity(intent)
+                } else{
+                    Toast.makeText(this,"${it.exception?.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
 
     override fun onBackPressed() {
         super.onBackPressed()
--- app/src/main/java/kampus/merdeka/I/Do/app/register_Activity.kt
+++ app/src/main/java/kampus/merdeka/I/Do/app/register_Activity.kt
@@ -3,27 +3,81 @@ package kampus.merdeka.I.Do.app
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
+import kampus.merdeka.I.Do.app.databinding.ActivityRegisterBinding
+import java.util.regex.Pattern
 
 class register_Activity : AppCompatActivity() {
 
-    var daftar_akun : Button? = null
+    lateinit var auth: FirebaseAuth
+    lateinit var binding: ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        setContentView(binding.root)
 
 
         lighStatusBar(window)
 
-        daftar_akun = findViewById(R.id.btn_daftar)
+        auth = FirebaseAuth.getInstance()
 
-        daftar_akun?.setOnClickListener {
-            startActivity(Intent(this@register_Activity, Register_Berhasil_Activity::class.java))
+
+        binding.btnDaftar.setOnClickListener{
+            val email = binding.editEmailRegister.text.toString()
+            val password = binding.passwordRegister.text.toString()
+
+
+            if (email.isEmpty()){
+                binding.editEmailRegister.error = "Email Wajib Diisi"
+                binding.editEmailRegister.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                binding.editEmailRegister.error = "Email Tidak Valid"
+                binding.editEmailRegister.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (password.isEmpty()){
+                binding.passwordRegister.error = "Password Wajib Diisi"
+                binding.passwordRegister.requestFocus()
+                return@setOnClickListener
+            }
+
+
+
+            if (password.length < 8){
+                binding.passwordRegister.error = "Password Minimal 8 Karakter"
+                binding.passwordRegister.requestFocus()
+                return@setOnClickListener
+            }
+
+
+            RegisterFirebase(email,password)
         }
 
+
+
+    }
+
+    private fun RegisterFirebase(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email,password)
+            .addOnCompleteListener(this){
+                if (it.isSuccessful){
+                    Toast.makeText(this, "Regiater Berhasil", Toast.LENGTH_SHORT).show()
+                    val intent = Intent (this,Register_Berhasil_Activity::class.java)
+                    startActivity(intent)
+                } else{
+                    Toast.makeText(this,"${it.exception?.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
